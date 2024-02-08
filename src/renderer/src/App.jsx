31bc02866c2c10dd9e30e19d4d4cf6e8a0a5b989
FalasFry <william.persson@live.se>
2024-02-08 10:20:22 +0100

    next card

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import FlashCard from "./components/FlashCard";
+import testFile from "./assets/test.json"
 
 function App() {
 
-  const [ cards, setCards ] = useState([]);
+  const [ cards, setCards ] = useState(Object.entries(testFile));
   const [ cardIndex, setCardIndex ] = useState(0);
 
+  function nextCard(){
+    cardIndex < cards.length-1 ? setCardIndex(curr => curr += 1) : setCardIndex(0);
+  }
+  function prevCard(){
+    cardIndex > 0 ? setCardIndex(curr => curr -= 1) : setCardIndex(cards.length-1);
+  }
 
   return (
-    <div className="container">
+    <div>
 
-      <div className="flashCard">
-        <div className="flashCard-inner">
-          <div className="frontCard">
-            <button>Hint</button>
-            <h2>Question</h2>
-          </div>
-          <div className="backCard">
-            <h5>Answer</h5>
-            <p>...</p>
-          </div>
+      <div className="navBarContainer">
+        <div className="navBar">
+          <button>Load Deck of Cards</button>
+          <button>Create New Card</button>
         </div>
+      </div>
+
+      <div className="container">
+
+        <FlashCard question={cards[cardIndex][1].Question} answer={cards[cardIndex][1].Answer} hint={cards[cardIndex][1].Hint} />
+
+      </div>
+
+      <div className="panel">
+        <button onClick={prevCard}>Prev</button>
+        {cardIndex+1  +" / " +cards.length}
+        <button onClick={nextCard}>Next</button>
       </div>
 
     </div>
---
+++ src/renderer/src/components/FlashCard.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function FlashCard({ question, answer, hint }){
+  
+  const [clicked, setClicked] = useState(false);
+
+  function cardPress(){
+    setClicked(curr => curr = !curr);
+  }
+
+    return(
+      <div className={clicked ? "flashCard-c" : "flashCard"} onClick={cardPress}>
+        <div className="flashCard-inner">
+          <div className="frontCard">
+            <h2>{question}</h2>
+          </div>
+          <div className="backCard">
+            <h5>Answer</h5>
+            <p>{answer}</p>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default FlashCard;
